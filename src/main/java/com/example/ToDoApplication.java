8c9e1fe63c1839af193d0398669b273e979b55d3
Shaik Appapuram Firoz <157677351+ShaikAppapuramFiroz@users.noreply.github.com>
2025-09-01 15:24:21 +0530

    Update ToDoApplication.java

--- src/main/java/com/example/ToDoApplication.java
+++ src/main/java/com/example/ToDoApplication.java
@@ -6,8 +6,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ToDoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ToDoApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        System.out.println("DATABASE_URL=" + System.getenv("DATABASE_URL"));
+        System.out.println("DATABASE_USERNAME=" + System.getenv("DATABASE_USERNAME"));
+		System.out.println("DATABASE_USERNAME=" + System.getenv("DATABASE_PASSWORD"));
+        SpringApplication.run(ToDoApplication.class, args);
+    }
 }
+
