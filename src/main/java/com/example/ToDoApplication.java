73115a292bfbd079f1bd9d04ceee6654eb98cc25
Shaik Appapuram Firoz <157677351+ShaikAppapuramFiroz@users.noreply.github.com>
2025-09-01 15:40:33 +0530

    Update ToDoApplication.java

--- src/main/java/com/example/ToDoApplication.java
+++ src/main/java/com/example/ToDoApplication.java
@@ -9,7 +9,7 @@ public class ToDoApplication {
     public static void main(String[] args) {
         System.out.println("DATABASE_URL=" + System.getenv("DATABASE_URL"));
         System.out.println("DATABASE_USERNAME=" + System.getenv("DATABASE_USERNAME"));
-		System.out.println("DATABASE_USERNAME=" + System.getenv("DATABASE_PASSWORD"));
+		System.out.println("DATABASE_PASSWORD=" + System.getenv("DATABASE_PASSWORD"));
         SpringApplication.run(ToDoApplication.class, args);
     }
 }
